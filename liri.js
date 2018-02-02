20240e38d5c251c00e4212c4533feb3471634944
Julia Lidstrom <juleslidstrom@gmail.com>
2018-02-02 17:37:48 -0600

    Working Spotify API

--- liri.js
+++ liri.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 var authKeys = require("./keys.js");
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
+var fs = require("fs");
 
 var spotify = new Spotify(authKeys.spotify);
 var client = new Twitter(authKeys.twitter);
@@ -12,6 +13,7 @@ var nodeArgs = process.argv;
 var action = nodeArgs[2];
 var value = nodeArgs[3];
 var movieName = "";
+var songName = "";
 
 
 /* Main switch function that calls the appropriate function depending on the main argument */
@@ -24,14 +26,14 @@ switch (action) {
     case "my-tweets":
       twitterRequest();
       break;
-  /*
+  
     case "spotify-this-song":
-      spotifyRequests();
+      songSelection();
       break;
   
     case "do-what-it-says":
       randomTextCommand();
-      break; */
+      break; 
   }
 
   /* OMDB API */
@@ -89,6 +91,50 @@ function twitterRequest(){
             console.log(myTweetsText);
             console.log("==========================");
         })
-       
-     });
+    });
 }
+
+function songSelection() {
+    if (value==undefined){
+        // songName = "The&20Sign"
+        songName = "The Sign";
+        var artist = "Ace of Base";
+        spotifyRequest();    
+    } else {
+         for (var i=3; i<nodeArgs.length; i++){
+             if(i>3 && i<nodeArgs.length){
+                 songName = songName + "%20"+nodeArgs[i];
+                 console.log(songName);
+             }else {
+                 songName+=nodeArgs[i];
+             }
+             spotifyRequest();    
+         }    
+     }
+ }
+
+function spotifyRequest(){
+   
+   
+    spotify.search({ type: 'track', query: songName, limit:1}, function(err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        } else {
+            var returnedSong = data.tracks.items;
+            var songArtist = returnedSong[0].album.artists[0].name;
+            var songName = returnedSong[0].name;
+            var songLink = returnedSong[0].href;
+            var albumName = returnedSong[0].album.name;
+            console.log("========================="); 
+            console.log(songArtist); 
+            console.log(songName); 
+            console.log(songLink); 
+            console.log(albumName); 
+            console.log("========================="); 
+        }
+       
+     
+      });
+}
+
+
